07675dbcb5371cf64b5da503713d7866dacb9629
KamilJagoda <kamil.jagoda@gate-software.com>
2018-10-22 12:23:54 +0200

    Navbar

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
 
+    var menuShowButton = $('#show-menu'),
+        menuHideButton = $('#hide-menu'),
+        menuWrapper = $('.navbar__wrapper');
+
+    menuShowButton.on('click', function(e) {
+        e.preventDefault();
+        menuWrapper.addClass('open');
+    });
+    menuHideButton.on('click', function(e) {
+        e.preventDefault();
+        menuWrapper.removeClass('open');
+    });
+
 
 });
